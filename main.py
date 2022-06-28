65e146d7c487a217304be43d749aa4951b544cae
Jeremy Tan <jtkw@Jeremys-Air.attlocal.net>
2022-06-28 01:09:20 -0700

    add done func

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import datetime
+from distutils.log import error
 from telegram.ext import Updater, CommandHandler
 from sheets import *
 import pytz
 from pytz import timezone
 import logging
+
+chores_map = {
+    'trash': 1,
+    'upstairs_toilet': 2,
+    'downstairs_toilet': 3,
+    'common_toilet': 4,
+    'common_floor': 5
+}
 
 updater = Updater(token='5138358228:AAF50OgNglW72Y69TFmeRTHGqQ9n1HVdsVo',
     use_context=True) # receives updates from Telegram
@@ -17,49 +26,59 @@ def chores_message(date):
     if not chores:
         return 'Chores not found.'
     else:
-        return (
-            '🗑️ Trash: %s\n'
+        return ((
+            '🗑️ Trash: %s\n' 
             '🚽 Upstairs toilet: %s\n'
             '🚽 Downstairs toilet: %s\n'
             '🚽 Common toilet: %s\n'
             '🧹 Common floor: %s'
-        ) % (chores[0], chores[1], chores[2], chores[3], chores[4])
+        ) % (chores[0], chores[1], chores[2], chores[3], chores[4])).replace('DONE', 'DONE ✅')
+
+def chores_help(update,context):
+    help_message = '/help - help\n/chores <OPTIONAL: prev/next> - list of chores for current week, specify prev or next for other weeks\n/chores_done <trash/upstairs_toilet/downstairs_toilet/common_toilet/common_floor> - set specified chore for current week as done'
+    context.bot.send_message(chat_id=update.effective_chat.id, text=help_message)
+
 
 # chores handler
 def chores(update, context):
     today = datetime.datetime.now(tz=pytz.utc)
     today = today.astimezone(timezone('US/Pacific'))
+    if len(context.args) > 0:
+        arg = context.args[0]
+        if arg == 'prev':
+            today += datetime.timedelta(-7)
+        if arg == 'next':
+            today += datetime.timedelta(7)
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Invalid option, please specify prev, next or no options')
+            return
+
     todayString = today.strftime("%B %d, %Y")
     message = 'Retrieving chores for week of %s...' % todayString
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     message = chores_message(today)
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
-def last_week_chores(update, context):
-    today = datetime.datetime.now(tz=pytz.utc)
-    today = today.astimezone(timezone('US/Pacific'))
-    todayString = today.strftime("%B %d, %Y")
-    message = 'Retrieving chores for week of %s...' % todayString
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    message = chores_message(today  + datetime.timedelta(-7))
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-def next_week_chores(update, context):
-    today = datetime.datetime.now(tz=pytz.utc)
-    today = today.astimezone(timezone('US/Pacific'))
-    todayString = today.strftime("%B %d, %Y")
-    message = 'Retrieving chores for week of %s...' % todayString
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    message = chores_message(today  + datetime.timedelta(7))
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
 def chores_done(update, context):
-
+    error_message = 'Please follow the format: /chores_done <trash/common_floor/upstairs_toilet/lower_toilet/common_toilet>'
+    if len(context.args) > 0:
+        what = context.args[0]
+        if what in chores_map:
+            today = datetime.datetime.now(tz=pytz.utc)
+            today = today.astimezone(timezone('US/Pacific'))
+            updateDoneChore(today, chores_map[what])
+            message = '{} marked as done'.format(what)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=error_message)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=error_message)
 
 chores_handler = CommandHandler('chores', chores) # associate /start with above fn
-last_week_chores_handler = CommandHandler('last_week_chores', last_week_chores) # associate /start with above fn
-next_week_chores_handler = CommandHandler('next_week_chores', next_week_chores) # associate /start with above fn
+chores_done_handler = CommandHandler('chores_done', chores_done) # associate /start with above fn
+chores_help_handler = CommandHandler('chores_help', chores_help) # associate /start with above fn
 dispatcher.add_handler(chores_handler)
-dispatcher.add_handler(last_week_chores_handler)
-dispatcher.add_handler(next_week_chores_handler)
+dispatcher.add_handler(chores_done_handler)
+dispatcher.add_handler(chores_help_handler)
 updater.start_polling()
+
--- sheets.py
+++ sheets.py
@@ -3,42 +3,63 @@ from apiclient import discovery
 import pytz
 from pytz import timezone
 import datetime
+import pygsheets
 tz = pytz.utc
 
-def getSchedule():
-    api_key = 'AIzaSyBeh_CnQNh8-_041kUcKUbPBTgBSgMcYVs' # hide as secret
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                        'version=v4')
-    service = discovery.build(
+sheets_api_key = 'AIzaSyBeh_CnQNh8-_041kUcKUbPBTgBSgMcYVs' # hide as secret'
+spreadsheet_id = '1tiYLMKpJFCfbTie_yqzmJYoE9IWWd4545BW9Wviyt_U' # hide as secret
+discovery_url = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
+service = discovery.build(
         'sheets',
         'v4',
         http=httplib2.Http(),
-        discoveryServiceUrl=discoveryUrl,
-        developerKey=api_key)
+        discoveryServiceUrl=discovery_url,
+        developerKey=sheets_api_key)
+gc = pygsheets.authorize(service_file='/Users/jtkw/Desktop/tele-bot.json')
+sh = gc.open('Chores')
+wks = sh[0]
 
-    spreadsheetId = '1tiYLMKpJFCfbTie_yqzmJYoE9IWWd4545BW9Wviyt_U' # hide as secret
-    rangeName = 'Schedule!A3:F50'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
-    date_chores = {}
-    for row in values:
-        dateStr = row[0].split(", ")[1]
-        print('Key: {}'.format(dateStr))
-        date_chores[dateStr] = row[1:len(row)]
 
-    return date_chores
 
-def getWeekChores(queryDate):
+def getRow(queryDate): # return row of interest
+    # get the first column
+    firstColumRange = 'Schedule!A1:A50'
+    firstColumnResult = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id, range=firstColumRange).execute()
+    firstColumnValues = firstColumnResult.get('values', [])
+    date_row = {}
+    for index, row in enumerate(firstColumnValues):
+        split = row[0].split(", ")
+        dateStr = split[1] if len(split) > 1 else split[0]
+        date_row[dateStr] = index + 1
+    return date_row[queryDate]
+
+
+
+def getChores(queryDate):
+    row = getRow(queryDate)
+    choresRange = 'Schedule!B{}:F{}'.format(row, row)
+    choresResult = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id, range=choresRange).execute()
+    choresValues = choresResult.get('values', [])[0]
+    return choresValues
+
+def updateDoneChore(date, choreIndex):
+    row = getRow(dateToQuery(date))
+    column = chr(ord('`')+choreIndex+1)
+    updateRange = '{}{}'.format(column, row)
+    curName = wks.cell(updateRange).value
+    wks.update_value(updateRange, '{} - DONE'.format(curName))
+
+def getWeekChores(date):
     # convert date to nearest-previous monday
-    queryDay = int(datetime.datetime.strftime(queryDate, '%w')) # 0 = sun, 1 = mon etc
-    offsetDays = 6 if queryDay == 0 else queryDay - 1
-    mondayDate = queryDate - datetime.timedelta(offsetDays)
-    keyStr = mondayDate.strftime('%-d %B %Y')
-    print('Looking for: {}'.format(keyStr))
-    schedule = getSchedule()
-    if keyStr in schedule:
-        return schedule[keyStr]
-    else:
-        return None
+    
+    print('Looking for: {}'.format(dateToQuery(date)))
+    return getChores(dateToQuery(date))
+
+def dateToQuery(date):
+    queryDay = int(datetime.datetime.strftime(date, '%w')) # 0 = sun, 1 = mon etc
+    offsetDays = 6 if queryDay == 0 else queryDay - 1 if queryDay != 1 else 7
+    mondayDate = date - datetime.timedelta(offsetDays)
+    return mondayDate.strftime('%-d %B %Y')
 
